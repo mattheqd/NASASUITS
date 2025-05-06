da338949a38a20ce095759bc99c052ab80ee4f4b
Andrew Hwang <pandy430@gmail.com>
2025-05-06 16:17:22 -0700

    finishing up some ui

--- Assets/Scripts/MainPanelToggle.cs
+++ Assets/Scripts/MainPanelToggle.cs
@@ -1,13 +1,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class TabManager : MonoBehaviour
 {
     [SerializeField] private List<GameObject> panels;
     [SerializeField] private List<Button> tabButtons;
+    [SerializeField] private List<RawImage> tabIcons;
+    [SerializeField] private List<TextMeshProUGUI> tabTexts;
     [SerializeField] private Color tabNormalColor = new Color(0.1686f, 0.1765f, 0.1843f, 1f);
     [SerializeField] private Color tabSelectedColor = new Color(0.0314f, 0.8667f, 1f, 1f);
+    private static readonly Color iconTextSelectedColor = new Color(37f/255f, 37f/255f, 37f/255f, 1f);
+    private static readonly Color iconTextNormalColor = Color.white;
 
     void Start()
     {
@@ -26,6 +31,8 @@ public class TabManager : MonoBehaviour
             bool active = (i == index);
             panels[i].SetActive(active);
             tabButtons[i].image.color = active ? tabSelectedColor : tabNormalColor;
+            tabIcons[i].color = active ? iconTextSelectedColor : iconTextNormalColor;
+            tabTexts[i].color = active ? iconTextSelectedColor : iconTextNormalColor;
         }
     }
 }
