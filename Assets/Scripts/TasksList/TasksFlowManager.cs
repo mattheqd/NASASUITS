fdb1e3b782495ce9b1432843c895daa2c2b4351e
Andrew Hwang <pandy430@gmail.com>
2025-05-06 17:58:34 -0700

    procedures_ui

--- Assets/Scripts/TasksList/TasksFlowManager.cs
+++ Assets/Scripts/TasksList/TasksFlowManager.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
+using System.Collections.Generic;
 
 public class TasksFlowManager : MonoBehaviour
 {
@@ -8,6 +10,11 @@ public class TasksFlowManager : MonoBehaviour
     [SerializeField] private GameObject procedurePanel;
     [SerializeField] private Button egressButton;
     [SerializeField] private Button startButton;
+    [SerializeField] private Button backButton;
+    [SerializeField] private StepItem stepItemPrefab;
+    [SerializeField] private Transform stepsContainer;
+
+    private List<Procedure> firstThreeProcedures;
 
     private void Awake()
     {
@@ -15,17 +22,59 @@ public class TasksFlowManager : MonoBehaviour
         procedurePanel.SetActive(false);
         egressButton.onClick.AddListener(ShowTaskInfo);
         startButton.onClick.AddListener(ShowProcedure);
+        backButton.onClick.AddListener(ShowTasksList);
     }
 
     private void ShowTaskInfo()
     {
         tasksListPanel.SetActive(false);
         taskInfoPanel.SetActive(true);
+        PopulateFirstSteps("Egress");
     }
 
     private void ShowProcedure()
     {
         taskInfoPanel.SetActive(false);
         procedurePanel.SetActive(true);
+    }
+
+    private void ShowTasksList()
+    {
+        taskInfoPanel.SetActive(false);
+        procedurePanel.SetActive(false);
+        tasksListPanel.SetActive(true);
+    }
+
+    private void PopulateFirstSteps(string procedureName)
+    {
+        Debug.Log($"TasksFlowManager: PopulateFirstSteps called for '{procedureName}'");
+        if (ProcedureManager.Instance == null)
+        {
+            Debug.LogError("TasksFlowManager: ProcedureManager.Instance is null");
+            return;
+        }
+        var proc = ProcedureManager.Instance.GetProcedure(procedureName);
+        if (proc == null)
+        {
+            Debug.LogError($"TasksFlowManager: Procedure '{procedureName}' not found");
+            return;
+        }
+        if (stepItemPrefab == null)
+        {
+            Debug.LogError("TasksFlowManager: stepItemPrefab is not assigned");
+            return;
+        }
+        if (stepsContainer == null)
+        {
+            Debug.LogError("TasksFlowManager: stepsContainer is not assigned");
+            return;
+        }
+        foreach (Transform child in stepsContainer) Destroy(child.gameObject);
+        int count = Mathf.Min(3, proc.instructionSteps.Count);
+        for (int i = 0; i < count; i++)
+        {
+            var item = Instantiate(stepItemPrefab, stepsContainer);
+            item.SetStep(i + 1, proc.instructionSteps[i].instructionText);
+        }
     }
 } 
