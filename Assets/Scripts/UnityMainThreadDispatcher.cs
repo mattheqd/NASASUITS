3896f9f94741a0eee2ab98a05ffa8b5ca83996f4
Jaslavie <jasmineqw45@gmail.com>
2025-05-10 20:07:02 -0700

    updated _instance

--- Assets/Scripts/UnityMainThreadDispatcher.cs
+++ Assets/Scripts/UnityMainThreadDispatcher.cs
@@ -13,18 +13,18 @@ public class UnityMainThreadDispatcher : MonoBehaviour {
     
     // create a public static instance of the dispatcher which can be accessed by other scripts
     // the public instance allows other scripts to call the dispatcher without direct access
-    public static UnityMainThreadDispatcher Instance { get; private set; }
+    public static UnityMainThreadDispatcher _instance { get; private set; }
 
     // when initialized, check if the instance is already set
     // destroy existing instances to reset the thread
     private void Awake()
     {
-        if (Instance != null && Instance != this)
+        if (_instance != null && _instance != this)
         {
             Destroy(gameObject);
             return;
         }
-        Instance = this;
+        _instance = this;
         DontDestroyOnLoad(gameObject);
     }
     // process all incoming actions on the main thread
@@ -41,5 +41,4 @@ public class UnityMainThreadDispatcher : MonoBehaviour {
             _executionQueue.Enqueue(action);
         }
     }
-
 }
