e38fca9cbc249919f2a9ebb259dc801db36cc966
Jaslavie <jasmineqw45@gmail.com>
2025-05-17 15:29:28 -0700

    vars for telemetry scripts

--- Assets/Scripts/Errors/TelemetryMonitor.cs
+++ Assets/Scripts/Errors/TelemetryMonitor.cs
@@ -6,6 +6,7 @@ using System;
 
 public class TelemetryMonitor : MonoBehaviour
 {
+    // ------- Variables -------
     // creates a new alert when a telemetry value is off-nominal
     [Serializable]
     public class TelemetryAlert {
@@ -16,3 +17,27 @@ public class TelemetryMonitor : MonoBehaviour
         public string message; // Message to display to the user
         public DateTime timestamp; // Time the alert was created
     }
+
+    // events systems can subscribe to to be notified when a telemetry value falls under that range
+    // create a new unity event that is notified when a telemetry value falls under the caution range
+    // creates a list of all alerts which is processed by the telemetry monitor
+    public UnityEvent<TelemetryAlert> onCautionDetected = new UnityEvent<TelemetryAlert>();
+    public UnityEvent<TelemetryAlert> onCriticalDetected = new UnityEvent<TelemetryAlert>();
+    public UnityEvent<TelemetryAlert> onReturnToNominal = new UnityEvent<TelemetryAlert>();
+
+    // creates a list of all thresholds which is compared to the telemetry values
+    // ex: [{"parameterName": "Temperature", "minNominal": 20, "maxNominal": 30, "minCaution": 15, "maxCaution": 25}]
+    [SerializeField] 
+    private List<TelemetryThreshold> thresholds = new List<TelemetryThreshold>();
+
+    // dictionary tracks current alert status for each parameter
+    // alerts stored in a dict with parameter name as key and alert as the value
+    // the alert will persist on the screen until the status reaches "nominal" by which "onReturnToNominal" is called
+    private Dictionary<string, Dictionary<string, TelemetryAlert>> alerts = new Dictionary<string, Dictionary<string, TelemetryAlert>>();
+
+    // Current DCU data for each astronaut
+    private DCUData dcuData1; // EVA1
+    private DCUData dcuData2; // EVA2
+
+    //------- Functions -------
+}
