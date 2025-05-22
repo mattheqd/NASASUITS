6847a27a100d628b98a2b1427e1287b74b7a0a84
Andrew Hwang <pandy430@gmail.com>
2025-05-21 20:49:10 -0500

    websocket

--- Assets/Scripts/Websocket/WebSocketClient.cs
+++ Assets/Scripts/Websocket/WebSocketClient.cs
@@ -322,7 +322,7 @@ public class WebSocketClient : MonoBehaviour
 {
     private WebSocket ws;
     private Dictionary<string, List<Action<object>>> messageHandlers = new Dictionary<string, List<Action<object>>>();
-    private string serverUrl = "wss://ae04-2600-100d-b034-1760-db5-4105-37b1-5cc6.ngrok-free.app/ws";
+    private string serverUrl = "ws://192.168.51.171:5185/ws";
     
 
     private bool isConnected = false;
